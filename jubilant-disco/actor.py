5da419e859bfbe752c97693c591551fc59f2c251
Petya <vegvaripeter21@gmail.com>
2025-08-11 19:10:00 +0200

    adsf

--- jubilant-disco/actor.py
+++ jubilant-disco/actor.py
@@ -5,7 +5,7 @@ from base_table import BaseTable
 
 class ActorBase(SQLModel):
     money: int = 0
-    products: list[ProductBase] = Relationship(back_populates="product")
+    products: list[ProductBase] = Relationship(back_populates="owner")
 
 
 class Actor(ActorBase, BaseTable, table=True):
--- jubilant-disco/product.py
+++ jubilant-disco/product.py
@@ -1,12 +1,15 @@
-from sqlmodel import SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 from .good import Good
 from .actor import Actor
 from .base_table import BaseTable
 
 
 class ProductBase(SQLModel):
-    good: Good
-    owner: Actor
+    good_id: int = Field(default=None, foreign_key="good.id")
+    good: Good = Relationship(back_populates="good")
+    
+    owner_id: int = Field(default= None, foreign_key="owner.id")
+    owner: Actor = Relationship(back_populates="product")
     quantity: int = 0
     price: float
 
