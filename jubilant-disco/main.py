a8307c29bbdb55f2d3011dd54acc0b4d57722652
Petya <vegvaripeter21@gmail.com>
2025-08-11 20:47:37 +0200

    moved to main

--- jubilant-disco/main.py
+++ jubilant-disco/main.py
@@ -1,5 +1,40 @@
+from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from .person import Person, PersonCreate
+from .base_table import BaseTable
+
+
 def main() -> None:
-    print("Hello from jubilant-disco!")
+
+    sqlite_file_name = "database.db"
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+    def create_db_and_tables():
+        SQLModel.metadata.create_all(engine)
+
+    app = FastAPI()
+
+    @app.on_event("startup")
+    def on_startup():
+        create_db_and_tables()
+
+    @app.post("/person/", response_model=BaseTable)
+    def create_person(person: PersonCreate):
+        with Session(engine) as session:
+            db_people = Person.model_validate(person)
+            session.add(db_people)
+            session.commit()
+            session.refresh(db_people)
+            return db_people
+
+    @app.get("/people/", response_model=list[BaseTable])
+    def read_heroes():
+        with Session(engine) as session:
+            people = session.exec(select(Person)).all()
+            return people
 
 
 if __name__ == "__main__":
--- jubilant-disco/test.py
+++ jubilant-disco/test.py
@@ -1,7 +1,3 @@
-from fastapi import FastAPI
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-from .person import Person, PersonCreate
-from .base_table import BaseTable
 
 
 #class HeroBase(SQLModel):
@@ -21,38 +17,3 @@ from .base_table import BaseTable
 #class HeroPublic(HeroBase):
 #    id: int
 
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-app = FastAPI()
-
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
-
-
-@app.post("/person/", response_model=BaseTable)
-def create_person(person: PersonCreate):
-    with Session(engine) as session:
-        db_people = Person.model_validate(person)
-        session.add(db_people)
-        session.commit()
-        session.refresh(db_people)
-        return db_people
-
-
-@app.get("/people/", response_model=list[BaseTable])
-def read_heroes():
-    with Session(engine) as session:
-        people = session.exec(select(Person)).all()
-        return people
